373a49bb38b0fcc5f6513d8fa31be384f31b9f93
ConstanzaMacarena <constanza.cerda.g@mail.pucv.cl>
2022-05-30 17:02:56 -0400

    Agregue registro
    Cree una nueva carpeta en app llamada registro necesita algunas modificaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PublishComponent } from './publish/publish.component';
 import { UsComponent } from './us/us.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
-  {path:'home', component:HomeComponent},
-  {path:'publicar', component:PublishComponent},
-  {path:'nosotros', component:UsComponent},
-  {path:'login', component:LoginComponent}
+  {path:'', component:HomeComponent},  
+    {
+      path:'publicar', component:PublishComponent
+    },
+    {
+      path:'nosotros', component:UsComponent
+    },
+    {
+      path:'login', component:LoginComponent
+    },
+    {
+      path:'registro', component:RegistroComponent
+    },
 ];
 
 @NgModule({
