01a6bfd31fb283a2e2db5c141194646005326d32
Germán Tabilo <german.tabilo.m@gmail.com>
2022-05-31 17:28:37 -0400

    cambios pendientes registro y eliminación de icono snapshat

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -10,8 +10,46 @@ export class RegistroComponent implements OnInit {
 
   registrocontrol : FormGroup;
   constructor() {
-    this.registrocontrol = new FormGroup({})
+    this.registrocontrol = new FormGroup({
+      names : new FormControl('', [
+        Validators.required,
+        Validators.maxLength(50)
+      ]),
+      email : new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(50)
+      ]),
+      pass : new FormControl('', [
+        Validators.required,
+        Validators.nullValidator,
+        Validators.minLength(10),
+        Validators.maxLength(30)
+      ]),
+      lastname : new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(50)
+      ]),
+      phone : new FormControl('', [
+        Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(9),
+        Validators.pattern("^[0-9]")
+      ]),
+      repeatpass : new FormControl('', [
+        Validators.required,
+        Validators.required,
+        Validators.nullValidator,
+        Validators.minLength(10),
+        Validators.maxLength(30)
+      ]),
+
+
+    })
    }
+
+
 
   ngOnInit(): void {
   }
