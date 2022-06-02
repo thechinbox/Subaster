dd9069b4156ab226bda0a2f3fdec2c92b197a2bf
Germán Tabilo <german.tabilo.m@gmail.com>
2022-06-02 18:41:52 -0400

    import html browser components

--- src/app/data/Services/menu-items.service.ts
+++ src/app/data/Services/menu-items.service.ts
@@ -26,7 +26,39 @@ export class MenuItemsService {
      {
        _id:'6296d9a9c30c38f00016051b',
        categoria:'Artefactos sanitarios y gasfitería'
-     }
+     },
+     {
+      _id:'6296d9e9c30c38f00016051c',
+      categoria:'Revestimientos y estucos'
+     },
+     {
+      _id:'6296da0dc30c38f00016051d',
+      categoria:'Maderas y muebles'
+     },
+     {
+      _id:'6296da17c30c38f00016051e',
+      categoria:'Equipos y herramientas'
+     },
+     {
+      _id:'6296da52c30c38f000160521',
+      categoria:'Pinturas y accesorios'
+     },
+     {
+      _id:'6296da5bc30c38f000160522',
+      categoria:'Cerámicos y adhesivos'
+     },
+     {
+      _id:'6296da66c30c38f000160523',
+      categoria:'Puertas y ventanas'
+     },
+     {
+      _id:'6296da70c30c38f000160524',
+      categoria:'Residuos peligrosos'
+     },
+     {
+      _id:'6296da77c30c38f000160525',
+      categoria:'Seguridad'
+     },
     ]
     return menuItems;
    }
