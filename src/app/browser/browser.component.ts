3041f7a240013ba204b5d32e17f3d3b937747ba6
Germán Tabilo <german.tabilo.m@gmail.com>
2022-06-04 18:11:33 -0400

    cambios services

--- src/app/browser/browser.component.ts
+++ src/app/browser/browser.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItemsService } from '../data/Services/menu-items.service';
 import { MenuItems } from '../data/Interfaces/menu-items';
-import { PublicationService } from '../data/Services/publication.service';
 import { Publication } from '../data/Interfaces/publication';
+import { PublicationService } from '../data/Services/publication.service';
+
 
 @Component({
   selector: 'app-browser',
@@ -13,18 +14,27 @@ export class BrowserComponent implements OnInit {
 
   MenuItems : MenuItems[];
   publicaciones : Publication[];
-  constructor( private _menuItemsService : MenuItemsService) {
+  constructor( private _menuItemsService : MenuItemsService , private publicationService: PublicationService) {
     this.MenuItems = this._menuItemsService.obtenerItems();
     this.publicaciones = this._menuItemsService.obtenerPublicaciones();
    }
 
   ngOnInit(): void {
-    /* Recargar template cuando se aplique la función checkboxSelected */
+    this.obtenerPublicacion();
   }
 
 
   checkboxSelected(){
     /* Desarrollar funcion de filtro */
+  }
+
+
+  private async obtenerPublicacion(){
+    let publication = this.publicationService.PUBLISH
+  this.publicationService.getPost().subscribe(data => {
+    console.log(data);
+   })
+
   }
 
 }
