ec655531a7a2460d78604fe2bf1db2621647266e
dfizeit <german.tabilo.m@gmail.com>
2022-06-08 19:35:10 -0400

    cambios home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {PublicationService} from '../data/Services/publication.service';
 import { Publish } from '../data/Interfaces/publish';
+import { MediaContent } from 'back-end/Interfaces/media-content';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Direction } from 'readline';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +12,17 @@ import { Publish } from '../data/Interfaces/publish';
 })
 export class HomeComponent implements OnInit {
   lista : Array<Publish>
-
+  imgPublicaciones : Array<MediaContent>;
   constructor(private _publicationService:PublicationService) {
     this.lista = new Array;
+    this.imgPublicaciones = new Array;
   }
 
   homePublications(){
    this._publicationService.getPost().subscribe(data => {
       this.lista = data;
-      console.log(this.lista);
+      this.imgPublicaciones = data.url;
+      console.log(this.imgPublicaciones)
     })
   }
 
