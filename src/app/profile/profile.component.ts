e75b097c0ef6f7ea418f2047159a53ae79b8f297
thechinbox <rodrigo.cardenas.v@mail.pucvl.cl>
2022-06-21 05:48:39 -0400

    update profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -13,18 +13,20 @@ export class ProfileComponent implements OnInit {
 
   usuario:User;
   profileInformation:FormGroup;
-
+  edit:boolean;
+  ids = ["nombre", "apellidos", "correo" , "direccion.direccion", "celular"]
+  editObject:any = {};
+  editando:any = []
   constructor(private _user:UserService) {
+    this.edit = false;
     this.usuario = this._user.getUser();
     this.profileInformation = new FormGroup({
-      nombres : new FormControl('', [
+      nombre : new FormControl('', [
         Validators.required,
-        Validators.minLength(10),
         Validators.maxLength(50)
       ]),
       apellidos : new FormControl('', [
         Validators.required,
-        Validators.minLength(10),
         Validators.maxLength(50)
       ]),
       correo : new FormControl('', [
@@ -49,12 +51,47 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this._user.getEmiter().subscribe( data => {
-      this.usuario = this._user.getUser()
-    })
+    if(sessionStorage.getItem("id") != null){
+      this._user.getEmiter().subscribe( data => {
+        this.usuario = this._user.getUser()
+      })
+      for(let id of this.ids){
+        let aux:any = document.getElementById(id);
+        aux.disabled = true;
+      }
+      setTimeout(() => {
+        let aux:any = document.getElementById('nombre');
+        if(aux.value == ""){
+          for(let id of this.ids){
+            let aux:any = document.getElementById(id);
+            aux.value = eval("this.usuario."+id)
+          }
+        }
+      }, 500);
+    }
 
     //llamar función para limpiar el formulario sin enviar
     this.resetForm();
   }
-
+  editar(property:any){
+    let aux:any = document.getElementById(property)
+    aux.disabled = false;
+    this.editObject[property] = property
+    this.editando.push(property)
+  }
+  removeEdit(property:any){
+    this.profileInformation?.get(property)?.setErrors(null) 
+    let aux:any = document.getElementById(property)
+    aux.disabled = true;
+    aux.value = eval("this.usuario."+ property)
+    delete this.editObject[property]
+    for (let index = 0; index < this.editando.length; index++) {
+      if(this.editando[index] == property){
+        delete this.editando[index]
+        this.editando = this.editando.filter(function (el:any) {
+          return el != null;
+        });
+      }
+    }
+  }
 }
