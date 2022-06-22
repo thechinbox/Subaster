f405fe116bd961a2af6f606589a258b2b144ab83
thechinbox <rodrigo.cardenas.v@mail.pucvl.cl>
2022-06-22 01:28:30 -0400

    update prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+
 export const environment = {
-  production: true
+  production: true,
+  hostname:"http://137.184.139.154:8080/",
+  firebaseConfig : {
+    apiKey: "AIzaSyB-1ZOWLmkBWFhplKvpy-mR3hoPnd0zGAE",
+    authDomain: "subaster-4fb24.firebaseapp.com",
+    databaseURL: "https://subaster-4fb24-default-rtdb.firebaseio.com",
+    projectId: "subaster-4fb24",
+    storageBucket: "subaster-4fb24.appspot.com",
+    messagingSenderId: "404375391813",
+    appId: "1:404375391813:web:9d2feffed130485c161d2c"
+  }
 };
